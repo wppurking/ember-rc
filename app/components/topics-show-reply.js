8726f38f5db2beb6d8d896a4f279b60ab00a3f06
Wyatt Pan <wppurking@gmail.com>
2015-05-29 02:31:21 +0800

    * reply 的编号.

--- app/components/topics-show-reply.js
+++ app/components/topics-show-reply.js
@@ -3,6 +3,9 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   tagName: 'div',
   classNames: 'reply',
+  number: function() {
+    return this.attrs.num.value + 1;
+  }.property(),
 
   actions: {
     scrollToit(reply) {
