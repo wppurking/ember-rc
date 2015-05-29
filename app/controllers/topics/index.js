44d2f23300b0b245122f1be27d16601a098de216
Wyatt Pan <wppurking@gmail.com>
2015-05-29 22:28:52 +0800

    * 完成从 controller 中收集 page 参数, 传递到 route 中去 server 端获取最新数据.

--- app/controllers/topics/index.js
+++ app/controllers/topics/index.js
@@ -1,7 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  queryParams: ['page'],
+  page: 1,
+
+
   last_actived: function() {
+    console.log('controller.last_actived trigger. offset:' + this.get('offset') + "::offsetEnd:" + this.get('limit'));
+    // this.model 代表, route 中一次请求后, 设置到 controller 中的 model.
+    // this.store 代表, 整个前端中已经持久化的数据
     return this.model.sortBy('updated_at');
-  }.property()
+  }.property('page') // 每次 page 变化, last_actived 都变化一次
 });
