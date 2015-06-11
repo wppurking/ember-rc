cfcfed9afc3fa8d6ffcc5f2d4840cf8da33ede62
wyatt <wppurking@gmail.com>
2015-06-11 12:01:21 +0800

    找到几种可以在 Controller 中排序的方法

--- app/controllers/topics/index.js
+++ app/controllers/topics/index.js
@@ -6,17 +6,30 @@ export default Ember.Controller.extend(SR, {
   queryParams: ['page'],
   page: 1,
 
+
+  // 可变的排序参数
+  topicsSort: ['updated_at:desc'],
+
   // 是否可以向前页
   prevDisabled: function() {
     return this.get('page') > 1 ? '' : 'disabled';
   }.property('page'),
 
+  /* sort 的几种写法.
   last_actived: function() {
     console.log('controller.last_actived trigger. offset:' + this.get('offset') + "::offsetEnd:" + this.get('limit'));
     // this.model 代表, route 中一次请求后, 设置到 controller 中的 model.
     // this.store 代表, 整个前端中已经持久化的数据
-    return this.model.sortBy('updated_at').reverse();
+    // TODO 1: 自己手写 API 来处理.
+    //return this.model.sortBy('updated_at').reverse();
+
+    // TODO 2: 手写 property 来处理.
+    return this.model;
   }.property('page', 'model'), // 每次 page 变化, last_actived 都变化一次
+  */
+
+  // TODO 3: 使用 Ember.computed.sort 来处理.
+  last_actived: Ember.computed.sort('model', 'topicsSort'),
 
 
   actions: {
