1b3f2eabb31ca6b4bfc4fd790b3f403468b094c8
wyatt <wppurking@gmail.com>
2015-06-19 13:05:37 +0800

    * 完成对 ruby-china 通过 password 获取 api

--- sinatra/serv.rb
+++ sinatra/serv.rb
@@ -1,12 +1,20 @@
-require "sinatra"
+require 'bundler'
+Bundler.require
 
 before do
-   headers 'Access-Control-Allow-Origin' => '*', 
-           'Access-Control-Allow-Methods' => ['OPTIONS', 'GET', 'POST', 'PUT'],
-           'Access-Control-Max-Age' => '1728000',
-           'Access-Control-Allow-Credentials' => '1728000'
+  headers 'Access-Control-Allow-Origin' => '*',
+          'Access-Control-Allow-Methods' => ['OPTIONS', 'GET', 'POST', 'PUT'],
+          'Access-Control-Max-Age' => '1728000',
+          'Access-Control-Allow-Credentials' => '1728000'
 end
 
-get "/" do
-  erb :index
+post "/login" do
+  content_type :json
+  post_data = {
+    grant_type: 'password',
+    username: params[:username],
+    password: params[:password]
+  }
+  res = HTTParty.post('https://ruby-china.org/oauth/token', body: post_data)
+  MultiJson.dump(res.merge(code: res.code))
 end
