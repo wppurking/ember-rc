4ea7fab25a38d1ac4a1a92027fdf1d493913f050
wyatt <wppurking@gmail.com>
2015-06-30 16:50:49 +0800

    * 创建 topic

--- app/controllers/topics/new.js
+++ app/controllers/topics/new.js
@@ -2,6 +2,10 @@ import Ember from 'ember';
 import splitSection from 'ember-rc/utils/node-section';
 
 export default Ember.Controller.extend({
+
+  topicContent: '',
+
+
   selectContent: function() {
     console.log('selectContent');
     var sectionMap = splitSection(this.model);
