684a82edb7db1e52bc66d1aacbef8004bc2c9e66
wyatt <wppurking@gmail.com>
2015-08-07 00:58:51 +0800

    * 使用 At.js PS: 还未解决变动的值传递到 Component 中的问题.

--- app/components/ex-text.js
+++ app/components/ex-text.js
@@ -5,5 +5,23 @@ export default Ember.TextArea.extend({
   didInsertElement() {
     // 目标: 将 element.heigh 设置成为 scrollHeight 的高度, scrollHeight 为包含内容后的总高度
     this.$().textareaAutoSize();
+    this.initAtWho();
+  },
+
+  initAtWho: function() {
+    if(!Ember.isBlank(this.attrs.replies)) {
+      console.log(`reply counts: ${this.attrs.replies.value.get('length')}`);
+      var commenters = Ember.A();
+      this.attrs.replies.value.forEach((r) => {
+        commenters.push(r.get('user.login'));
+      });
+
+      this.$().atwho({
+        at: "@",
+        data: commenters.uniq(),
+        search_key: "search",
+        tpl: "<li data-value='${login}'>${login} <small>${name}</small></li>"
+      });
+    }
   }
 });
