29e6c6392349bb7d434c00dab8acdc7469a1727f
wyatt <wppurking@gmail.com>
2015-08-12 02:39:19 +0800

    * update docker branch build shell script

--- docker_branch_build.sh
+++ docker_branch_build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rm -rf tmp/ && rm -rf dist/ && ember build -prod && git checkout dockerfile && git add -f dist/ && git commit -am 'update dist/' && git push origin dockerfile && git checkout master
+rm -rf tmp/ && rm -rf dist/ && ember build -prod && mv dist dist_bak && git checkout dockerfile && rm -rf dist && mv dist_bak dist && git add -f dist/ && git commit -am 'update dist/' && git push origin dockerfile && git checkout master
