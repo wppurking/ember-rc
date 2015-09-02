6dbf76133ad7cef5051dae45f3b4fe2d7715dc4a
wyatt <wppurking@gmail.com>
2015-09-03 01:31:12 +0800

    * 提示"喜欢"功能.

--- app/components/topics-show-reply.js
+++ app/components/topics-show-reply.js
@@ -13,8 +13,14 @@ export default Ember.Component.extend(SR, {
   },
 
   actions: {
-    scrollToit(reply) {
-      this.scrollTo($("#reply-" + reply.get('id')));
+    scrollToit() {
+      this.scrollTo($("#reply-" + this.reply.get('id')));
+    },
+
+    likeIt() {
+      var msg = `喜欢评论(#${this.reply.get('id')})的功能还没有实现呢.`;
+      console.log(msg);
+      alert(msg);
     }
   }
 });
